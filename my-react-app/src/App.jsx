fd3094cf2f157b31098a44bac7aa1de97fa2e2fd
Tanuja Gunapooti <g.tanuja@wustl.edu>
2024-11-10 16:39:32 -0600

    linked board squares to array squares

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -7,8 +7,7 @@ function Square() {
     // console.log('clicked!');
   }
   return (
-  <button className="square" onClick={handleClick}>
-  {value}</button>);
+  <button className="square" onClick={handleClick}>{value}</button>);
 }
 
 export default function Board() {
@@ -17,18 +16,18 @@ export default function Board() {
     <>
       <div className="board-row">
         <Square value={squares[0]} />
-        <Square />
-        <Square />
+        <Square value={squares[1]}/>
+        <Square value={squares[2]}/>
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]}/>
+        <Square value={squares[4]}/>
+        <Square value={squares[5]}/>
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]}/>
+        <Square value={squares[7]}/>
+        <Square value={squares[8]}/>
       </div>
     </>
   );
